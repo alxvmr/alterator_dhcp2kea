556bd88749aa97530330cfe21a915a3f53d8eb5b
Stanislav Ievlev <inger@altlinux.org>
2009-03-18 14:18:35 +0300

    - add special dhcp-dns-address type with support of '*'

---
+++ alterator-dhcp/interfaces/guile/type/dhcp-dns-address.scm
@@ -0,0 +1,7 @@
+(define-module (alterator type dhcp-dns-address)
+    :use-module ((alterator type ipv4-address)  :renamer (symbol-prefix-proc 'ipv4-address:))
+    :export (type))
+
+(define (type v _)
+  (or (and (string? v) (string=? v "*"))
+      (ipv4-address:type v _)))
