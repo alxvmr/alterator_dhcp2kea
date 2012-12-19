0086d2205843163da736805fcff412b2985ae528
Mikhail Efremov <sem@altlinux.org>
2012-12-19 16:22:51 +0400

    Initial DHCPv6 support.

--- alterator-dhcp/type/dhcp-dns-address.scm
+++ alterator-dhcp/type/dhcp-dns-address.scm
@@ -1,7 +1,7 @@
 (define-module (type dhcp-dns-address)
-    :use-module ((type ipv4-address)  :renamer (symbol-prefix-proc 'ipv4-address:))
+    :use-module ((type ip-address)  :renamer (symbol-prefix-proc 'ip-address:))
     :export (type))
 
 (define (type v _)
   (or (and (string? v) (string=? v "*"))
-      (ipv4-address:type v _)))
+	  (ip-address:type v _)))
--- alterator-dhcp/ui/dhcp/ajax.scm
+++ alterator-dhcp/ui/dhcp/ajax.scm
@@ -8,7 +8,7 @@
 (define (ui-general-read)
   (catch/message
     (lambda()
-      (let ((data (woo-read-first "/dhcp")))
+      (let ((data (woo-read-first "/dhcp" 'ipv (form-value "ipv"))))
 	(form-update-value-list *parameters* data)
 	(form-update-visibility
 	  '("client_dns" "client_search")
@@ -19,6 +19,7 @@
     (lambda()
       (apply woo-write "/dhcp"
              'general #t
+			 'ipv (form-value "ipv")
              'language (form-value "language")
              (form-value-list *parameters*))
       (ui-general-read))))
@@ -27,7 +28,7 @@
   (form-update-value "new_static_ip" "")
   (form-update-value "new_static_mac" "")
   (form-update-value "new_static_hname" "")
-  (form-update-enum "static_name" (woo-list "/dhcp/avail_static")))
+  (form-update-enum "static_name" (woo-list "/dhcp/avail_static" 'ipv (form-value "ipv"))))
 
 ;; note: iface, ip_start and ip_end are used for config validation
 (define (ui-static-del)
@@ -35,6 +36,7 @@
     (lambda()
       (apply woo-write "/dhcp"
              'static_del #t
+			 'ipv (form-value "ipv")
              'language (form-value "language")
              (form-value-list '("static_name"
                                 "iface" "ip_start" "ip_end")))
@@ -46,44 +48,54 @@
     (lambda()
       (apply woo-write "/dhcp"
              'static_add #t
+			 'ipv (form-value "ipv")
              'language (form-value "language")
              (form-value-list '("new_static_ip" "new_static_mac" "new_static_hname"
                                 "iface" "ip_start" "ip_end")))
       (ui-static-read))))
 
 (define (ui-lease-read)
-  (form-update-enum "lease_name" (woo-list "/dhcp/avail_lease")))
+  (form-update-enum "lease_name" (woo-list "/dhcp/avail_lease" 'ipv (form-value "ipv"))))
 
 (define (ui-lease-fix)
   (catch/message
     (lambda()
       (apply woo-write "/dhcp"
             'lease_fix #t
+			'ipv (form-value "ipv")
             'language (form-value "language")
             (form-value-list '("lease_name"
                                "iface" "ip_start" "ip_end")))
       (ui-lease-read)
       (ui-static-read))))
 
+(define (update-ui)
+  (let* ((ipv (form-value "ipv"))
+		 (iface-list (woo-list "/dhcp/avail_iface" 'ipv ipv 'language (form-value "language"))))
+	(if (null? iface-list)
+	  (begin
+		(form-update-visibility "nostatic_page" #t)
+		(form-update-visibility "static_page" #f))
+	  (begin
+		(form-update-visibility "client_gw" (string=? ipv "4"))
+		;; lists
+		(form-update-enum "iface" iface-list)
+		;; read
+		(ui-general-read)
+		(ui-static-read)
+		(ui-lease-read)))))
+
 (define (init)
-  (let ((iface-list (woo-list "/dhcp/avail_iface" 'language (form-value "language"))))
-    (if (null? iface-list)
-        (begin
-         (form-update-visibility "nostatic_page" #t)
-         (form-update-visibility "static_page" #f))
-        (begin
-	  ;; lists
-	  (form-update-enum "iface" iface-list)
-	  (form-update-enum "client_time" (woo-list "/dhcp/avail_time" 'language (form-value "language")))
-	  ;; read
-	  (ui-general-read)
-	  (ui-static-read)
-	  (ui-lease-read)
-	  ;; buttons
-	  (form-bind "apply_button" "click" ui-general-write)
-	  (form-bind "reset_button" "click" ui-general-read)
-	  (form-bind "static_del_button" "click" ui-static-del)
-	  (form-bind "static_add_button" "click" ui-static-add)
-	  (form-bind "lease_fix_button" "click" ui-lease-fix)
-	  ))))
+  (form-update-enum "ipv" (woo-list "/dhcp/ipv" 'language (form-value "language")))
+  (form-update-value "ipv" "4")
+  (form-update-enum "client_time" (woo-list "/dhcp/avail_time" 'language (form-value "language")))
+  (update-ui)
+  ;; buttons
+  (form-bind "apply_button" "click" ui-general-write)
+  (form-bind "reset_button" "click" ui-general-read)
+  (form-bind "static_del_button" "click" ui-static-del)
+  (form-bind "static_add_button" "click" ui-static-add)
+  (form-bind "lease_fix_button" "click" ui-lease-fix)
+  (form-bind "ipv" "change" update-ui)
+  )
 
