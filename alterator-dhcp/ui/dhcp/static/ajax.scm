b29bfd9ebd76aa997d844db8d894c4977a9fa130
Mikhail Efremov <sem@altlinux.org>
2012-12-19 21:01:19 +0400

    ipv6: Fix 'static' page.

--- alterator-dhcp/ui/dhcp/static/ajax.scm
+++ alterator-dhcp/ui/dhcp/static/ajax.scm
@@ -4,20 +4,24 @@
     :export (init))
 
 (define (ui-read)
-  (let ((mac (form-value "mac")))
+  (let ((mac (form-value "mac"))
+		(ipv (form-value "ipv")))
     (form-update-value "mac" mac)
     (form-update-value-list
       '("new_static_ip" "new_static_hname")
-      (woo-read-first "/dhcp/static" 'mac mac))))
+      (woo-read-first "/dhcp/static" 'mac mac 'ipv ipv))))
 
 (define (ui-write)
   (catch/message
     (lambda()
       (apply woo-write "/dhcp/static"
-             (form-value-list '("language" "mac" "new_static_ip" "new_static_hname")))
+             (form-value-list '("ipv" "language" "mac" "new_static_ip" "new_static_hname")))
       (form-replace "/dhcp"))))
 
 (define (init)
+  (form-update-enum "ipv" (woo-list "/dhcp/ipv" 'language (form-value "language")))
+  (form-update-value "ipv" "4")
   (ui-read)
   (form-bind "apply" "click" ui-write)
-  (form-bind "reset" "click" ui-read))
+  (form-bind "reset" "click" ui-read)
+  (form-bind "ipv" "change" ui-read))
